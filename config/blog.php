773c07f0528f16e3202fae64831a39ee0e4fc1e1
Tony LAURENT <contact@tony-laurent.com>
2018-05-14 13:25:49 +0200

    Add blog config file

---
+++ config/blog.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blog Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your blog.
+    */
+
+    'name' => env('BLOG_NAME', 'My blog'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blog Description
+    |--------------------------------------------------------------------------
+    |
+    | This value is the description of your blog.
+    */
+
+    'description' => env('BLOG_DESCRIPTION', 'This is the description of my blog.'),
+
+];

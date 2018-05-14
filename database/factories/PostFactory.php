35ef77ee71f0c7dc5af3c6125170bcb0f35e9a5c
Tony LAURENT <contact@tony-laurent.com>
2018-05-14 13:21:55 +0200

    Add fake image

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,9 +3,16 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
+    $image = $faker->file(
+        resource_path('tests/photos'), 
+        storage_path('app/public/tmp'), 
+        false
+    );
+    
     return [
         'title' => $faker->sentence(),
         'summary' => $faker->paragraph(5, false),
-        'content' => $faker->text(2000)
+        'content' => $faker->text(2000),
+        'image' => "tmp/{$image}"
     ];
 });

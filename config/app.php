f9fc6100d619f681db9dd83f9301b910a53c3cde
Tony LAURENT <contact@tony-laurent.com>
2018-06-10 11:01:54 +0200

    Disable theme provider

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
          * Package Service Providers...
          */
         GrahamCampbell\Markdown\MarkdownServiceProvider::class,
-        Verbal\Theme\Bootstrap\ThemeServiceProvider::class,
+        //~ Verbal\Theme\Bootstrap\ThemeServiceProvider::class,
 
         /*
          * Application Service Providers...

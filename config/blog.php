df77410f7c8088bb045666dfcc10fab7c03d9996
Tony LAURENT <contact@tony-laurent.com>
2018-06-16 16:13:18 +0200

    Remove useless blog environment config

--- config/blog.php
+++ config/blog.php
@@ -10,7 +10,7 @@ return [
     | This value is the name of your blog.
     */
 
-    'name' => env('BLOG_NAME', 'My blog'),
+    'name' => 'My blog',
 
     /*
     |--------------------------------------------------------------------------
@@ -20,6 +20,6 @@ return [
     | This value is the description of your blog.
     */
 
-    'description' => env('BLOG_DESCRIPTION', 'This is the description of my blog.'),
+    'description' => 'This is the description of my blog.'
 
 ];
